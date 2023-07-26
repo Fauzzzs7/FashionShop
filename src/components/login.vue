5ef39289188c774c41b21775b65703767889a20f
xxbatokxx <iqbalrisqi700@gmail.com>
2023-07-26 16:13:09 +0700

    commit

--- src/components/login.vue
+++ src/components/login.vue
@@ -5,82 +5,53 @@
     <!-- Login component -->
     <div class="flex" style="width: 100vw; height: 100%">
       <!-- Background form -->
-      <div
-        class="flex content-center justify-center bg-white"
-        style="width: 100vw; height: 100vh"
-      >
-        <img src="../assets/logo_web.svg" style="width: 40px; height: 40px; margin-top: 20px;"/>
+      <div class="flex content-center justify-center bg-white" style="width: 100vw; height: 100vh">
+        <img src="../assets/logo_web.svg" style="width: 40px; height: 40px; margin-top: 20px;" />
         <!-- login form -->
-        <div
-          class="flex flex-wrap content-center justify-center rounded-lg mt-56 font-desc bg-secondGrey"
-          style="width: 24rem; height: 32rem"
-        >
+        <div class="flex flex-wrap content-center justify-center mt-56 rounded-lg font-desc bg-secondGrey"
+          style="width: 24rem; height: 32rem">
           <div class="w-72">
             <!-- Heading -->
-            <h1 class="text-xxl font-bold text-center">Welcome Back</h1>
-            <p class="text-gray-400 text-center">
+            <h1 class="font-bold text-center text-xxl">Welcome Back</h1>
+            <p class="text-center text-gray-400">
               Let's find your fashion style
             </p>
 
             <!-- Form -->
             <form class="mt-4" @submit.prevent="submitForm">
               <div class="mb-3">
-                <label class="mb-2 block text-xs font-semibold">Email</label>
-                <input
-                  type="email"
-                  v-model="email"
-                  placeholder="Enter your email" required
-                  class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
-                />
+                <label class="block mb-2 text-xs font-semibold">Email</label>
+                <input type="email" v-model="email" placeholder="Enter your email" required
+                  class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500" />
               </div>
 
               <div class="mb-3">
-                <label class="mb-2 block text-xs font-semibold">Password</label>
-                <input
-                  type="password"
-                  v-model="password"
-                  placeholder="*****" required
-                  class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
-                />
+                <label class="block mb-2 text-xs font-semibold">Password</label>
+                <input type="password" v-model="password" placeholder="*****" required
+                  class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500" />
               </div>
 
-              <div class="mb-3 flex content-center">
-                <input
-                  id="remember"
-                  type="checkbox"
-                  class="mr-1 checked:bg-purple-700"
-                />
-                <label for="remember" class="mr-auto text-xs font-semibold"
-                  >Remember password</label
-                >
-                <a href="#" class="text-xs font-semibold text-black"
-                  >Forgot password?</a
-                >
+              <div class="flex content-center mb-3">
+                <input id="remember" type="checkbox" class="mr-1 checked:bg-purple-700" />
+                <label for="remember" class="mr-auto text-xs font-semibold">Remember password</label>
+                <a href="#" class="text-xs font-semibold text-black">Forgot password?</a>
               </div>
 
               <div class="mb-3">
-                <button
-                  type="submit"
-                  class="mb-1.5 block w-full text-center text-white bg-dark hover:bg-gray-600 px-2 py-1.5 rounded-md"
-                >
+                <button type="submit"
+                  class="mb-1.5 block w-full text-center text-white bg-dark hover:bg-gray-600 px-2 py-1.5 rounded-md">
                   Login
                 </button>
               </div>
               <div class="mb-3 ">
                 <button
-                  class="flex flex-wrap justify-center w-full border border-grey hover:border-gray-500 px-2 py-1.5 rounded-md bg-white"
-                >
-                <google-signin-button
-                  :client-config="googleConfig"
-                  @success="handleGoogleSignInSuccess"
-                  @failure="handleGoogleSignInFailure"
-                >
-                <img
-                    class="w-5 mr-2"
-                    src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
-                  />
-                </google-signin-button>
-                  
+                  class="flex flex-wrap justify-center w-full border border-grey hover:border-gray-500 px-2 py-1.5 rounded-md bg-white">
+                  <google-signin-button :client-config="googleConfig" @success="handleGoogleSignInSuccess"
+                    @failure="handleGoogleSignInFailure">
+                    <img class="w-5 mr-2"
+                      src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" />
+                  </google-signin-button>
+
                   Log in with Google
                 </button>
               </div>
@@ -88,9 +59,7 @@
 
             <!-- Footer -->
             <div class="text-center">
-              <span class="text-xs text-gray-400 font-semibold"
-                >Don't have account?</span
-              >
+              <span class="text-xs font-semibold text-gray-400">Don't have account?</span>
               <router-link to="/signup">
                 <a href="#" class="text-xs font-semibold text-black">Sign up</a>
               </router-link>
@@ -100,15 +69,9 @@
       </div>
 
       <!-- Login banner -->
-      <div
-        class="flex flex-wrap rounded-r-md"
-        style="width: 100%; height: 100vh"
-      >
-        <img
-          class="w-full h-full bg-no-repeat bg-cover mr-0"
-          style="width: 100vw; height: 100vh"
-          src="../assets/images/AW22 1.jpg"
-        />
+      <div class="flex flex-wrap rounded-r-md" style="width: 100%; height: 100vh">
+        <img class="w-full h-full mr-0 bg-no-repeat bg-cover" style="width: 100vw; height: 100vh"
+          src="../assets/images/AW22 1.jpg" />
       </div>
     </div>
   </div>
@@ -143,20 +106,20 @@ export default {
     },
     submitForm() {
       const userData = {
-      email: this.email,
-      password: this.password,
-    };
+        email: this.email,
+        password: this.password,
+      };
       axios.post('http://127.0.0.1:8000/api/login', userData)
-      .then(response => {
-        const { user, token } = response.data;
-        localStorage.setItem('token', token);
-        this.isLoggedIn = true;
-        this.$router.push('/');
-        
-      })
-      .catch(error => {
-        console.error(error.response.data);
-      });
+        .then(response => {
+          const { user, token } = response.data;
+          localStorage.setItem('token', token);
+          this.isLoggedIn = true;
+          this.$router.push('/');
+
+        })
+        .catch(error => {
+          console.error(error.response.data);
+        });
     }
   }
 }
