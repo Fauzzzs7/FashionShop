c0785e5c924ce43d82807f59fe57a8eda41fa7b9
Fauzzzs7 <fauzsyabana68@gmail.com>
2023-10-29 22:39:21 +0700

    benerin tag

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,12 +1,12 @@
 <script>
-import { VueGoogleSigninButton } from 'vue-google-signin-button';
+import { VueGoogleSigninButton } from "vue-google-signin-button";
 import axios from "axios";
 export default {
   data() {
     return {
-      name: '',
-      email: '',
-      password: ''
+      name: "",
+      email: "",
+      password: "",
     };
   },
   components: {
@@ -14,29 +14,32 @@ export default {
   },
   methods: {
     signupWithGoogle() {
-        axios.get('http://127.0.0.1:8000/sanctum/csrf-cookie').then(response => {
-    // Login...
+      axios
+        .get("http://127.0.0.1:8000/sanctum/csrf-cookie")
+        .then((response) => {
+          // Login...
           window.location = "http://127.0.0.1:8000/api/signup/google";
-          });
+        });
     },
-  submitForm() {
-    const userData = {
-      name: this.name,
-      email: this.email,
-      password: this.password,
-    };
-    axios.post("http://127.0.0.1:8000/api/signup", userData)
-      .then(response => {
-        // Handle the successful response (e.g., show a success message, redirect, etc.)
-        this.$router.push('/');
-        const token  = response.data.token;
-        localStorage.setItem('token', token);
-        isLoggedIn = true;
-      })
-      .catch(error => {
-        // Handle the error response (e.g., display validation errors, show an error message, etc.)
-        // console.error(error.response.data);
-      });
+    submitForm() {
+      const userData = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+      };
+      axios
+        .post("http://127.0.0.1:8000/api/signup", userData)
+        .then((response) => {
+          // Handle the successful response (e.g., show a success message, redirect, etc.)
+          this.$router.push("/");
+          const token = response.data.token;
+          localStorage.setItem("token", token);
+          isLoggedIn = true;
+        })
+        .catch((error) => {
+          // Handle the error response (e.g., display validation errors, show an error message, etc.)
+          // console.error(error.response.data);
+        });
     },
     onGoogleSignUpSuccess(googleUser) {
       // Retrieve the necessary user details from the googleUser object
@@ -49,40 +52,43 @@ export default {
         email,
         imageUrl,
       };
-      axios.post('http://127.0.0.1:8000/api/signup/google', userData)
-        .then(response => {
+      axios
+        .post("http://127.0.0.1:8000/api/signup/google", userData)
+        .then((response) => {
           // Handle the successful response (e.g., show a success message, redirect, etc.)
-          const token  = response.data.token;
-          localStorage.setItem('token', token);
+          const token = response.data.token;
+          localStorage.setItem("token", token);
           // this.isLoggedIn = true;
-          this.$router.push('/');
+          this.$router.push("/");
           console.log(response.data.message);
         })
-        .catch(error => {
+        .catch((error) => {
           // Handle the error response (e.g., display validation errors, show an error message, etc.)
         });
-    }, onGoogleSignUpError(error) {
+    },
+    onGoogleSignUpError(error) {
       // Handle the Google sign-up error
       console.error(error);
     },
   },
 };
-
-
 </script>
 
 <template>
   <div class="flex flex-wrap">
     <div class="flex" style="width: 100vw; height: 100%">
-
-
-      <div class="flex content-center justify-center bg-white" style="width: 100vw; height: 100vh"><router-link to="/">
+      <div
+        class="flex content-center justify-center bg-white"
+        style="width: 100vw; height: 100vh">
+        <router-link to="/">
           <button>
-            <img src="../assets/logo_web.svg" style="width: 40px; height: 40px; margin-top: 20px;" />
+            <img
+              src="../assets/logo_web.svg"
+              style="width: 40px; height: 40px; margin-top: 20px" />
           </button>
-
         </router-link>
-        <div class="flex flex-wrap content-center justify-center mt-56 rounded-lg font-desc bg-secondGrey"
+        <div
+          class="flex flex-wrap content-center justify-center mt-56 rounded-lg font-desc bg-secondGrey"
           style="width: 24rem; height: 34.5rem">
           <div class="w-72">
             <h1 class="font-bold text-center text-xxl">Welcome back</h1>
@@ -91,78 +97,61 @@ export default {
             </p>
 
             <form class="mt-4" @submit.prevent="submitForm">
-
-
               <div class="mb-3">
                 <label class="block mb-2 text-xs font-semibold">Username</label>
-                <input type="text" placeholder="Enter your username" v-model="name" required
+                <input
+                  type="text"
+                  placeholder="Enter your username"
+                  v-model="name"
+                  required
                   class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500" />
               </div>
 
-
               <div class="mb-3">
                 <label class="block mb-2 text-xs font-semibold">Email</label>
-                <input type="email" placeholder="Enter your email" v-model="email" required
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  v-model="email"
+                  required
                   class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500" />
               </div>
 
               <div class="mb-3">
                 <label class="block mb-2 text-xs font-semibold">Password</label>
-                <input type="password" placeholder="*****" v-model="password" required
+                <input
+                  type="password"
+                  placeholder="*****"
+                  v-model="password"
+                  required
                   class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500" />
               </div>
 
-              <div class="mb-3 ">
+              <div class="mb-3">
                 <div>
-                  <button type="submit"
+                  <button
+                    type="submit"
                     class="mb-1.5 block w-full text-center text-white bg-dark hover:bg-gray-600 px-2 py-1.5 rounded-md">
                     <u>Sign up</u>
                   </button>
                 </div>
               </div>
               <div>
-                <button @click="signupWithGoogle"
+                <button
+                  @click="signupWithGoogle"
                   class="flex flex-wrap justify-center w-full border border-grey hover:border-gray-500 px-2 py-1.5 rounded-md bg-white">
-                  <img class="w-5 mr-2"
+                  <img
+                    class="w-5 mr-2"
                     src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA" />
                   Sign Up with Google
                 </button>
-               </div>
-             </form>
-            
- 
-             <div class="text-center">
-               <span class="text-xs font-semibold text-gray-400"
-                 >Already have account? </span
-               >
-               <router-link to="/login">
-                 <a href="#" class="text-xs font-semibold text-black">Login</a>
-               </router-link>
-             </div>
-           </div>
-         </div>
-       </div>
- 
-       <div
-         class="flex flex-wrap rounded-r-md"
-         style="width: 100%; height: 100vh"
-       >
-         <img
-           class="w-full h-full bg-no-repeat bg-cover"
-           style="width: 100vw; height: 100vh"
-           src="../assets/images/AW221.jpg"
-         />
-       </div>
-     </div>
-   </div>
- </template>
- 
               </div>
             </form>
 
-
             <div class="text-center">
-              <span class="text-xs font-semibold text-gray-400">Already have account? </span>
+              <span class="text-xs font-semibold text-gray-400"
+                >Already have account?
+              </span>
               <router-link to="/login">
                 <a href="#" class="text-xs font-semibold text-black">Login</a>
               </router-link>
@@ -171,9 +160,13 @@ export default {
         </div>
       </div>
 
-      <div class="flex flex-wrap rounded-r-md" style="width: 100%; height: 100vh">
-        <img class="w-full h-full bg-no-repeat bg-cover" style="width: 100vw; height: 100vh"
-          src="../assets/images/AW22 1.jpg" />
+      <div
+        class="flex flex-wrap rounded-r-md"
+        style="width: 100%; height: 100vh">
+        <img
+          class="w-full h-full bg-no-repeat bg-cover"
+          style="width: 100vw; height: 100vh"
+          src="../assets/images/AW221.jpg" />
       </div>
     </div>
   </div>
