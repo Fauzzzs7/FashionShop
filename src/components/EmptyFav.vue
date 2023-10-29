2e02b4fe6a5f1f6cc4ebdd677414ae20133a830f
Fauzzzs7 <fauzsyabana68@gmail.com>
2023-10-29 22:48:25 +0700

    masih benerin typo

--- src/components/EmptyFav.vue
+++ src/components/EmptyFav.vue
@@ -16,13 +16,13 @@ export default {
 
 <template>
   <div class="container ms-[47px] mt-[35px]">
-    <img src="src/assets/bookmark.svg" alt="bookmark">
+    <img src="../assets/Bookmark.svg" alt="bookmark">
     <p class="flex ms-[50px] mt-[-32px] text-2xl font-dsc font-semibold">Favorit</p>
   </div>
 
   <div class="flex justify-center relative top-[38vh]">
     <router-link to="/addfav">
-      <img src="src/assets/Add.svg" alt="tambah ">
+      <img src="../assets/Add.svg" alt="tambah ">
       <p class="flex ms-[26px] mt-[15px]  text-2xl font-dsc font-semibold">Empty</p>
     </router-link>
   </div>
